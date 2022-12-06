8885d78fc5cc552ef85509359cd6e31d1ab23b09
Goran Kekez <gorankekez@gmail.com>
2022-12-06 15:53:49 +0100

    finished app

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Animated Navigation 2
+const toggle = document.getElementById('toggle');
+const nav = document.getElementById('nav');
+
+toggle.addEventListener('click', () => nav.classList.toggle('active'));
